29328c920f9cca5ff1234fd7903f2d46eeb30959
Corey Howell <cjthowell@gmail.com>
2016-02-09 08:28:15 -0500

    beautified the main stream table view, also need to work on converting json to Post objects. I believe Alamofire has a serialize method to do this?

---
+++ Crewlo/PostTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostTableViewCell.swift
+//  Crewlo
+//
+//  Created by Corey Howell on 2/7/16.
+//  Copyright © 2016 Crewlo. All rights reserved.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Crewlo/User.swift
@@ -0,0 +1,9 @@
+//
+//  User.swift
+//  Crewlo
+//
+//  Created by Corey Howell on 2/7/16.
+//  Copyright © 2016 Crewlo. All rights reserved.
+//
+
+import Foundation
